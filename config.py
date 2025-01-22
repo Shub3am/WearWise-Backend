794ba596c669d0ee1a0489ca716f701af81fd5b1
shub3am <shubhamvishwakarma0604@gmail.com>
2025-01-22 22:00:46 +0530

    added openai env

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 load_dotenv()
 class Config:
     GROQ_AI_KEY = os.environ.get('GROQ_AI_KEY')
+    OPEN_AI_KEY= os.environ.get('OPEN_AI_KEY')
+    OPEN_AI_ORG= os.environ.get('OPEN_AI_ORG')
